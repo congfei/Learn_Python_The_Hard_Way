e33078842cacf41e844085054e02bc8a492bd278
congfei <congfei314@gmail.com>
2013-08-26 11:39:10 +0800

    笨办法学习Pyhton

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#print "Hello World!"
+print "Hello Again"
+print "I Like Typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "注意上面的python打印语法，"
